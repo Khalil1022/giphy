2f913ac8c9919b5b523349b39e73e749d7f1d227
Khalil Bogle Thorpe <khalil.andall3@gmail.com>
2019-04-09 21:59:08 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -5,7 +5,19 @@
 
 $("#search-button").click(function(){
   
-  
-  
+ var searchTerm = $("input").val();
+ var input = $("input").val();
+
+
+$.ajax({
+  url: "https://api.giphy.com/v1/gifs/search?q=" + input + "&rating=pg&api_key=dc6zaTOxFJmzC",
+method: "GET",
+  success: function(response) {
+    console.log(response);
+    $('body').append("<img src="+ response.data[0].images.original.url + ">");
+  },
 });
+    });
+
+
 
